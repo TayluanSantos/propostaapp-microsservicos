c02758ef29759dc8d68686c3a60d0d1b23c1d06d
TayluanSantos <tayluanjesus@gmail.com>
2024-10-29 19:53:15 -0300

    feat: Adicionando a configuração do RabbitAdmin

--- src/main/java/com/tayluan/propostaapp/config/RabbitMQConfig.java
+++ src/main/java/com/tayluan/propostaapp/config/RabbitMQConfig.java
@@ -2,6 +2,10 @@ package com.tayluan.propostaapp.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,4 +30,16 @@ public class RabbitMQConfig {
     public Queue  criarFilaPropostaConcluidaMsNotificacao(){
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
     }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicalizarAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+
 }
